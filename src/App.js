e678b5e127fc3ee3095f0b31ff20d6b337f1c10f
JulieKodehode <julie.kodehode@gmail.com>
2022-11-04 14:24:19 +0100

    First commit, updated README.md, checking if App.js is functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// Checking if you still work.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>Hi are you working?</h1>
+			<h4>Yes you are!</h4>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/README.md
@@ -0,0 +1,4 @@
+# Remaking my first webpage with only simple html and css with react.
+
+Ideas for weekend work (all voluntary):
+Take one of your earlier designs and recreate them in a new React project.
