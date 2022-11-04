b8e938f77d0932ca0ea93e972156ebdd39032781
JulieKodehode <julie.kodehode@gmail.com>
2022-11-04 14:40:35 +0100

    Adding the structure of the page in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-// Checking if you still work.
-
 const App = () => {
 	return (
 		<div>
-			<h1>Hi are you working?</h1>
-			<h4>Yes you are!</h4>
+			{/* Title */}
+			<h1>Welcome to Julie's website</h1>
+
+			{/* Flexbox two rows. */}
+			<div>
+				{/* Photo to the left */}
+				<div>
+					<img src="" alt="" />
+				</div>
+
+				{/* Two divs to the right */}
+				<div>
+					{/* Intro box */}
+					<div>
+						<h3>Here are three facts about me:</h3>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+
+					{/* Info box */}
+					<div>
+						<h3>A short presentation of me:</h3>
+						<p></p>
+						<p>
+							Click <a href="">here</a> to see more
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
